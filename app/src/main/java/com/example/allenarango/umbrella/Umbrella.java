9757f85defc54061dfd222036b6177d8b7c146b0
allenarango <allen.arango@gmail.com>
2015-07-28 19:25:01 -0500

    Working

--- app/src/main/java/com/example/allenarango/umbrella/Umbrella.java
+++ app/src/main/java/com/example/allenarango/umbrella/Umbrella.java
@@ -1,5 +1,6 @@
 package com.example.allenarango.umbrella;
 
+import android.location.Location;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,6 +8,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,70 +24,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class Umbrella extends ActionBarActivity {
+public class Umbrella extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private class WebServiceTask extends AsyncTask<String, String, String> {
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            TextView textview = (TextView) findViewById(R.id.hello);
-            textview.setText("Should I take an umbrella today? "+s);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String useUmbrellaStr = "Don't know, sorry about that.";
-            HttpURLConnection urlConnection = null;
-
-            try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                useUmbrellaStr = useUmbrella(urlConnection.getInputStream());
-            } catch (IOException e) {
-                Log.e("MainActivity", "Error ", e);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-            }
-            return useUmbrellaStr;
-        }
-            protected String useUmbrella(InputStream in) {
-                //read and parse InputStream
-                return "Yes!"; //temporary
-            }
-    }
-    protected String useUmbrella(InputStream in) {
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new InputStreamReader(in));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line + "\n");
-            }
-            //JSON needs to be parsed here
-            Log.i("Returned data", stringBuilder.toString());
-        } catch (Exception e) {
-            Log.e("MainActivity", "Error", e);
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (final IOException e) {
-                    Log.e("PlaceholderFragment", "Error closing stream", e);
-                }
-            }
-        }
-        return "Don't know, sorry about that.";
-    }
+    protected GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_umbrella);
-        WebServiceTask webserviceTask = new WebServiceTask();
-        webserviceTask.execute("Allen");
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
@@ -103,4 +61,94 @@ public class Umbrella extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.i("info","onConnected");
+        WebServiceTask webserviceTask = new WebServiceTask();
+        webserviceTask.execute(String.valueOf(mCurrentLocation.getLatitude()),String.valueOf(mCurrentLocation.getLongitude()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("error","onConnectedFailed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    private class WebServiceTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            String useUmbrellaStr = "Don't know, sorry about that.";
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                useUmbrellaStr = useUmbrella(urlConnection.getInputStream());
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error ", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return useUmbrellaStr;
+        }
+        protected String useUmbrella(InputStream in) {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                Log.i("JSON Response", stringBuilder.toString());
+                //Parse JSON
+                JSONObject forecastJson = new JSONObject(stringBuilder.toString());
+                JSONArray weatherArray = forecastJson.getJSONArray("list");
+                JSONObject todayForecast = weatherArray.getJSONObject(0);
+
+                if (todayForecast.has("rain") || todayForecast.has("snow")) {
+                    return ("Yes");
+                }
+                else {
+                    return ("No");
+                }
+            }
+            catch (Exception e) {
+                Log.e("MainActivity", "Error", e);
+            }
+            finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    }
+                    catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return "Don't know, sorry about that.";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            TextView textview = (TextView) findViewById(R.id.hello);
+            textview.setText("Should I take an umbrella today? "+s);
+        }
+    }
+
 }
